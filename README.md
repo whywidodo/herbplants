afea797fc61a41c5877a9dc773542e0fbd2bcbe1
whywidodo <mr.wahyuwidodo@gmail.com>
2021-08-20 16:14:49 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Herbplant - Informasi Tanaman Herbal
+
+Ini adalah website sederhana untuk menampilkan informasi mengenai tanaman herbal yang ada di Indonesia.
---
+++ js/script.js
@@ -0,0 +1,43 @@
+let data = new URLSearchParams(window.location.search);
+let datax = data.get("page")
+$.ajax({
+    url: "./assets/doc/data_herbal.json",
+    type: "get",
+    dataType: "json",
+    success: function (detail) {
+        var htA = "";
+        if(datax== null || datax == 1){
+            nilaiAwal = 0; jumlahData = 10;
+        }else if(datax == 2){
+            nilaiAwal = 10; jumlahData = 20;
+        }else if(datax == 3){
+            nilaiAwal = 20; jumlahData = 30;
+        }else if(datax == 4){
+            nilaiAwal = 30; jumlahData = 40;
+        }else if(datax == 5){
+            nilaiAwal = 40; jumlahData = 50;
+        }else if(datax == 6){
+            nilaiAwal = 50; jumlahData = 60;
+        }
+        for (var i = nilaiAwal; i < jumlahData; i++) {
+            var namaTanaman = detail['daftar_tanaman'][i]['nama'];
+            var descTanaman = detail['daftar_tanaman'][i]['deskripsi'];
+            var imgTanaman  = detail['daftar_tanaman'][i]['image_url'];
+
+            htA+='<div class="card mb-3">';
+            htA+='<div class="row g-0">';
+            htA+=   '<div class="col-md-4">';
+            htA+=       '<img src="'+ imgTanaman +'" class="img-fluid rounded-start">';
+            htA+=   '</div>';
+            htA+=   '<div class="col-md-8">';
+            htA+=       '<div class="card-body">';
+            htA+=           '<h5 class="card-title">'+ namaTanaman +'</h5>';
+            htA+=           '<p class="card-text">'+ descTanaman +'</p>';
+            htA+=       '</div>'
+            htA+=   '</div>'
+            htA+='</div>';
+            htA+='</div>';
+        }
+        $(".divA").html(htA);
+    }
+});
